921d2fc1ed4eebeab7d539af6185317b593fca80
swastikasharma04 <147703659+swastikasharma04@users.noreply.github.com>
2025-03-14 10:51:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World. i am swastika\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
